37ab3536afb82bac10e7ff8d5c3dccb195e7df7a
Eduard <ed.kech2005@mail.ru>
2023-05-06 15:33:05 -0400

    couple changes

--- fusuma/mv_workspace.py
+++ fusuma/mv_workspace.py
@@ -3,11 +3,16 @@
 import os,sys
 
 if __name__ == "__main__":
-    current_workspace = os.popen("wmctrl -d | grep '*' | cut -d ' ' -f1").read()
+    current_workspace = int(os.popen("wmctrl -d | grep '*' | cut -d ' ' -f1").read())
 
+    print(current_workspace)
     if sys.argv[1] == "left":
-        os.popen(f"wmctrl -s {int(current_workspace) - 1}")
+        if current_workspace == 0:
+            exit()
+        os.system(f"xdotool key alt+{current_workspace}")
     elif sys.argv[1] == "right":
-        os.popen(f"wmctrl -s {int(current_workspace) + 1}")
+        if current_workspace == 8:
+            os.system("xdotool key alt+0")
+        os.system(f"xdotool key alt+{current_workspace+2}")
     else:
         print("ERROOOOR!!!!")
