4d1fa2aff40d889c2b15eae4d0beac39d55025b9
Eduard <ed.kechedzhiev@gmail.com>
2024-03-17 16:52:53 -0400

    doom

--- doom/config.el
+++ doom/config.el
@@ -1,47 +1,67 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+(setq user-full-name "John Doe"
+      user-mail-address "john@doe.com")
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
+(map! :leader
+      :desc "Close current buffer"
+      "q q" #'kill-this-buffer)
+
+(map! :leader
+      :desc "Save buffer"
+      "w" #'save-buffer)
+
+(map! :leader
+      :desc "Toggle Treemacs"
+      "e" #'treemacs)
+
+(map! :leader
+      :desc "Focus on Treemacs"
+      "E" #'treemacs-select-window)
+
+(map! :n "L" #'next-buffer
+      :n "H" #'previous-buffer)
+
+(map! :leader
+      :desc "find file"
+      "s f" #'find-file)
+
+(map! :leader
+      :desc "search current buffer"
+      "s s" #'consult-line)
+
+(map! :leader
+      :desc "Toggle comments"
+      "/" #'evilnc-comment-or-uncomment-lines)
+
+(map! :leader
+      :desc "format"
+      "l f" #'+format/region-or-buffer)
+
+(after! vertico
+  (map! :map vertico-map "TAB" #'vertico-next)
+  (map! :map vertico-map "<backtab>" #'vertico-previous))
+
+;;;; UNMAPPED DEFUALTS ;;;;
+(map! :leader
+      "b [" nil
+      "b ]" nil
+      "f s" nil
+      "f f" nil
+      "s b" nil
+      "c f" nil)
 
 
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
-;; (setq user-full-name "John Doe"
-;;       user-mail-address "john@doe.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-symbol-font' -- for symbols
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq doom-font (font-spec :family "Fira Code Nerd Font" :size 20))
 (setq display-line-numbers-type t)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
+(setq doom-theme 'catppuccin)
+(after! catppuccin
+  (setq catppuccin-flavor 'macchiato)
+  (catppuccin-reload))
+
+;; remove auto format
+(add-hook! 'before-save-hook #'+format/buffer)
+(remove-hook! 'before-save-hook #'+format/buffer)
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
